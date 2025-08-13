37d42bcc2fc7f8a2080c8687802d3b91f0da74b5
Pragati Gupta <pragatigupta168@gmail.com>
2025-08-13 15:40:21 +0530

    Updated FSD-REACT files

--- newApp/src/pages/hooksDemo2.jsx
+++ newApp/src/pages/hooksDemo2.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import axios from "axios";
 function HookDemo2()
 {
-    function formHandler(event)
+
+    const [formData , setFormData] = useState({});
+    const [msg, setMsg] = useState(null)
+
+    function inpHandler(e){
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+
+        setFormData({...formData ,[e.target.name]: e.target.value})
+        // console.log(formData);
+        
+    }
+
+    async function formHandler(event)
     {
         event.preventDefault()
+        // console.log(formData);
+        const res = await axios.post("http://localhost:8000/admin/addUser",formData)
+        console.log(res);
+        setMsg(res.data.msg)
+        
     }
     
     return(
@@ -13,21 +35,25 @@ function HookDemo2()
                     <table className="table table-bordered w-50 table-dark text-center">
                         <tbody>
                             <tr>
-                                <td>User Name</td><td><input type="text" name="unm"/></td>
+                                <td>User Name</td><td><input type="text" name="unm" onInput={inpHandler}/></td>
                             </tr>
                             <tr>
-                                <td>Password</td><td><input type="password" name="pwd"/></td>
+                                <td>Password</td><td><input type="password" name="pwd" onInput={inpHandler}/></td>
+                            </tr>
+                            <tr>
+                                <td>Email</td><td><input type="email" name="mailId" onInput={inpHandler}/></td>
                             </tr>
                             <tr>
                                 <td colspan='2'><input type="submit" name="subBtn"/></td>
                             </tr>
                         </tbody>
-
                     </table>
-
-
                 </form>
-                
+                    {msg && (
+                        <p className="alert alert-success w-50 mx-auto">
+                            {msg}
+                        </p>
+                    )}
             </div>
             
         </>
--- newApp/src/pages/viewUser.jsx
+++ newApp/src/pages/viewUser.jsx
@@ -1,7 +1,63 @@
-function ViewUser(){
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+function ViewUser()
+{
+    const [count, setCount] = useState(0)
+    const [count2, setCount2] = useState(100)
+
+    const [userData, setUserData] = useState([])
+
+    // useEffect(()=>
+    // {
+    // console.log("Hi From useEffect")
+    // },[ count2 ]
+    // )
+
+    async function fetchAllData(){
+        const res = await axios.get("http://localhost:8000/admin/showUser")
+        setUserData(res.data)
+    }
+
+    useEffect(()=>{
+        fetchAllData()
+    },[]
+    )
+
     return(
         <>
         <h1>ViewUser Component</h1>
+        {count}
+        <button onClick={()=>setCount(count+1)}>Button</button>
+
+        {count2}
+        <button onClick={()=>setCount2(count2+1)}>Button</button>
+
+        {userData.length>0 && (
+        <table className="table table-bordered table-striped w-75 mx-auto text-center table-light">
+            <thead className="table-dark">
+                <tr>
+                    <th>UserName</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Role</th>
+                    <th>Active</th>
+                </tr>
+            </thead>
+            <tbody>
+                {userData.map((user)=>(
+                    <tr>
+                        <td>{user.userName}</td>
+                        <td>{user.emailId}</td>
+                        <td>{user.password}</td>
+                        <td>{user.hasRole}</td>
+                        <td>{user.isActive ? 'Active':'Inactive'}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        
+        )}
         </>
     )
 }
